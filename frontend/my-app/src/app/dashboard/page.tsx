0c20f5090742468afd0d244a27cff272707a52b0
shopnilashik <shopnil.ashik@gmail.com>
2025-08-09 01:24:30 +0600

    stable

--- frontend/my-app/src/app/dashboard/page.tsx
+++ frontend/my-app/src/app/dashboard/page.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
-export default function Home() {
+export default function Dashboard() {
   return (
     <main className="flex flex-col justify-center h-full text-center gap-6 max-w-5xl mx-auto">
       <h1 className="text-5xl font-bold">Invoicipedia</h1>
       <p>
         <Button asChild>
-          <Link href="/dashboard">Sign In</Link>
+          <Link href="/dashboard">Dashboard</Link>
         </Button>
       </p>
     </main>
